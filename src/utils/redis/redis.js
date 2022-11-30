00b95199e63eca58262191aeff05cc6ef37551bf
nightkitty <2329464297@qq.com>
2022-11-30 12:46:01 +0800

    修改redis连接url

--- src/utils/redis/redis.js
+++ src/utils/redis/redis.js
@@ -3,13 +3,12 @@ const { redis } = require('../../../project.config')
 
 /**
  * redis://[[username][:password]@][host][:port][/db-number]
- * 写密码redis://:123456@127.0.0.1:6379/0 
- * 写用户redis://uername@127.0.0.1:6379/0  
+ * 写密码redis://:123456@127.0.0.1:6379/0
+ * 写用户redis://uername@127.0.0.1:6379/0
  * 或者不写密码 redis://127.0.0.1:6379/0
  * 或者不写db_number redis://:127.0.0.1:6379
  */
-const url = `redis://${redis.host}:${redis.port}/${redis.db}`
-console.log(url);
+const url = `redis://:${redis.pass}@${redis.host}:${redis.port}/${redis.db}`
 const redisClient = createClient({
   url: url
 });
