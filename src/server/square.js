6c7495529b3998b2cb0faacd6ac363528ed2bac7
liu-xinkai <2329464297@qq.com>
2023-04-16 15:10:38 +0800

    优化发布帖子API的逻辑

--- src/server/square.js
+++ src/server/square.js
@@ -7,6 +7,14 @@ const { MaxPictures } = require("../../project.config");
 const mime = require("mime-types");
 const { starPost: starPostNotice } = require("../server/notice");
 
+// 发布帖子，回滚所有已保存的图片
+const rollBackPictures = successPictures => {
+  // 回滚操作:删除successPictures中的图片
+  for (let item of successPictures) {
+    fs.unlink(path.join(__dirname, "../../media/squarePictures", item.filename), () => {});
+  }
+};
+
 class Square {
   // 发布帖子
   async publishPost(ctx) {
@@ -22,22 +30,23 @@ class Square {
     } else {
       const successPictures = []; // 成功保存的图片
       let errResult = null; // 保存错误信息
+
+      // 如果上传了图片就检查图片，然后将图片转换成文件保存到本地，最后向数据库插入数据
       if (Reflect.get(files, "pictures")) {
-        // 如果上传了图片就检查图片，然后将图片转换成文件保存到本地，最后向数据库插入数据
         const pictures = files.pictures instanceof Array ? files.pictures : [files.pictures];
         if (pictures.length > MaxPictures) {
           errResult = { code: 400, msg: "图片不能超过6张，请重新上传" };
         } else {
-          // const successPictures = [];
           for (const picture of pictures) {
+            const createdTime = Date.now();
             const splits = picture.originalFilename.split(".");
-            splits.splice(1, 0, `${+new Date()}`);
+            splits.splice(1, 0, `${createdTime}`);
             const filename = `${uid}_${splits.join(".").replace(/[.]/, "_")}`;
             const result = await streamWriter(
               picture.filepath,
               path.join(__dirname, "../../media/squarePictures", filename)
             );
-            if (result.status === "Done") successPictures.push({ filename, size: result.size });
+            if (result.status === "Done") successPictures.push({ filename, size: result.size, createdTime });
             else {
               errResult = { code: 400, msg: result.error };
               break;
@@ -46,10 +55,7 @@ class Square {
         }
         // 上传的图片中有部分没有保存成功
         if (pictures.length !== successPictures.length) {
-          // 回滚操作:删除successPictures中的图片
-          for (let item of successPictures) {
-            fs.unlink(path.join(__dirname, "../../media/squarePictures", item.filename), () => {});
-          }
+          rollBackPictures(successPictures);
           return (ctx.body = errResult);
         }
       }
@@ -61,33 +67,28 @@ class Square {
         ${+new Date()})
       `)
         .then(async result => {
+          const insertId = result.insertId; // 帖子ID
+
           if (successPictures.length) {
-            const insertId = result.insertId;
             const sqls = [];
             successPictures.forEach(picture => {
               sqls.push(`insert into meetu_square_pictures(pic_name, size, art_id, updated_time) 
-                     values("${picture.filename}",${picture.size},${insertId},"${+new Date()}");`);
+                     values("${picture.filename}",${picture.size},${insertId},"${picture.createdTime}");`);
             });
             await transaction(sqls)
-              .then(arrResult => {
-                ctx.body = { code: 200, msg: "发布成功" };
+              .then(() => {
+                ctx.body = { code: 200, msg: "发布成功", data: { postId: insertId } };
               })
-              .catch(err => {
-                // 回滚操作，将保存的图片删除
-                for (let item of successPictures) {
-                  fs.unlink(path.join(__dirname, "../../media/squarePictures", item.filename), () => {});
-                }
-                ctx.body = { code: 500, msg: "图片数据保存失败，请重新上传" };
+              .catch(() => {
+                rollBackPictures(successPictures);
+                ctx.body = { code: 500, msg: "图片上传失败" };
               });
           } else {
-            ctx.body = { code: 200, msg: "发布成功" };
+            ctx.body = { code: 200, msg: "发布成功", data: { postId: insertId } };
           }
         })
-        .catch(err => {
-          // 回滚操作，将保存的图片删除
-          for (let item of successPictures) {
-            fs.unlink(path.join(__dirname, "../../media/squarePictures", item.filename), () => {});
-          }
+        .catch(() => {
+          rollBackPictures(successPictures);
           ctx.body = { code: 500, msg: "帖子数据保存失败" };
         });
     }
